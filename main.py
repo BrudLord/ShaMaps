c4dbdaa900d48cf9b9ff740d986acf05f3c61f22
ProRobotnic <devsign@mail.ru>
2021-02-12 01:34:14 +0300

    Zadacha 2

--- main.py
+++ main.py
@@ -4,30 +4,44 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-response = requests.get(map_request)
+base_scale = 0.001
+scale_modifier = 1
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+def do_map_request(map_scale):
+    map_scale = str(map_scale) + ',' + str(map_scale)
+    map_request = str("http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=" + map_scale + "&l=map")
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
 
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+clock = pygame.time.Clock()
+running = True
+screen.blit(pygame.image.load(do_map_request(base_scale * scale_modifier)), (0, 0))
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            os.remove("map.png")
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == 1073741899 and scale_modifier > 1:
+                scale_modifier *= 0.5
+            elif event.key == 1073741902 and scale_modifier < 4096:
+                scale_modifier += scale_modifier
+            print(scale_modifier, end='->')
+            screen.blit(pygame.image.load(do_map_request(base_scale * scale_modifier)), (0, 0))
+        pygame.display.flip()
 pygame.quit()
-
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
