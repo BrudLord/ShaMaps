9d154ff76866d259869773aa3fdd48bd8e0edabe
brud <brudloord@yandex.ru>
2021-02-28 23:38:04 +0300

    Добавил поиск.

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ import requests
 import pygame
 
 
+class InputBox:
+    def __init__(self):
+        self.rect = pygame.Rect(10, 50, 90, 35)
+        self.color = pygame.Color(9, 190, 150)
+        self.text = ''
+        self.active = True
+        self.x = 10
+        self.y = 50
+        self.w = 100
+        self.h = 35
+
+    def handle_event(self, k):
+        if self.active:
+            if pygame.key.name(k) == 'return':
+                self.text = ''
+            elif pygame.key.name(k) == 'backspace':
+                self.text = self.text[:-1]
+            else:
+                if pygame.key.name(k) == 'space':
+                    self.text += ' '
+                elif len(pygame.key.name(k)) == 1:
+                    self.text += pygame.key.name(k)
+
+    def draw(self):
+        pygame.draw.rect(screen, pygame.Color(150, 150, 150), (self.x, self.y, self.w, self.h))
+        print_text(self.text, self.x, self.y, self.w, self.h)
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h), 2)
+
+
 class Button:
     def __init__(self, width, height, x, y, name):
         self.width = width
@@ -31,6 +61,13 @@ class Button:
                     map_vid = 'sat'
                 if self.name == 'Гибрид':
                     map_vid = 'sat,skl'
+                if self.name == 'F':
+                    global base_cord
+                    bc = base_cord[:]
+                    try:
+                        base_cord = [float(i) for i in find_coords(ib.text).split()]
+                    except Exception:
+                        base_cord = bc
         else:
             pygame.draw.rect(screen, self.active_color, (x, y, self.width, self.height))
         if len(self.name) == 1:
@@ -52,6 +89,7 @@ scale_change_modifier = 2 # How changes scale_modifier durning work of program
 base_cord = [37.530887, 55.703118]
 base_cord_change = 0.0005 # cord change = scale_modifier * base_cord_change
 map_vid = 'map'
+ib = InputBox()
 
 
 def do_map_request(cord, map_scale):
@@ -71,8 +109,32 @@ def do_map_request(cord, map_scale):
     return map_file
 
 
+def find_coords(txt):
+    toponym_to_find = txt
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        # обработка ошибочной ситуации
+        pass
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    return toponym["Point"]["pos"]
+
+
 # Инициализируем pygame
-buttons = [Button(35, 35, 10, 10, 'Схема'), Button(35, 35, 55, 10, 'Спутник'), Button(35, 35, 100, 10, 'Гибрид')]
+buttons = [Button(35, 35, 10, 10, 'Схема'), Button(35, 35, 55, 10, 'Спутник'), Button(35, 35, 100, 10, 'Гибрид'),
+           Button(15, 35, 120, 50, 'F')]
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 clock = pygame.time.Clock()
@@ -85,6 +147,7 @@ while running:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
+            ib.handle_event(event.key)
             # scale events
             if event.key == pygame.K_PAGEUP and scale_modifier > 1:
                 scale_modifier *= (1 /scale_change_modifier)
@@ -102,5 +165,6 @@ while running:
         screen.blit(pygame.image.load(do_map_request(base_cord, base_scale * scale_modifier)), (0, 0))
     for i in buttons:
         i.draw()
+    ib.draw()
     pygame.display.flip()
 pygame.quit()
